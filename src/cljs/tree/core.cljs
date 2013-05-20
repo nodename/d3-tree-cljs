e0c4e6b85acdcc62a24d9c9b720f11c1401586e3
Alan Shaw <alan@nodename.com>
2013-05-19 17:39:45 -0700

    Now can select horizontal or vertical tree layout and bezier or line-segment links

--- src/cljs/tree/core.cljs
+++ src/cljs/tree/core.cljs
@@ -1,311 +1,182 @@
-
 (ns tree.core
-      (:require [strokes :refer [d3]]))
+      (:require [clojure.zip :as zip]
+                [strokes :refer [d3]]))
 
-(strokes/bootstrap)
-
-    
 (defn log [& more]
     (.log js/console (apply str more)))    
     
 
-;; var m = [20, 120, 20, 120],
-;;    w = 1280 - m[1] - m[3],
-;;    h = 800 - m[0] - m[2],
-;;    i = 0,
-;;    root;    
-
-
-(def m [20 120 20 120])
-(def w (- 1280 (m 1) (m 3)))
-(def h (- 800 (m 0) (m 2)))
-;; not using this (def i 0)
-(def root)
-
-
-;; var tree = d3.layout.tree()
-;;    .size([h, w]);
+(strokes/bootstrap)
     
-    
-(def tree (-> d3 (.-layout) (.tree)
-            (.size [h w])))
-
-;; var diagonal = d3.svg.diagonal()
-;;    .projection(function(d) { return [d.y, d.x]; });
-
-
-(def diagonal (-> d3 (.-svg) (.diagonal)
-                (.projection (fn [d] [(.-y d) (.-x d)]))))
+(def svg-width 1280)
+(def svg-height 800)
+(def margin {:top 20 :left 120 :bottom 20 :right 120})
+(def tree-width (- svg-width (margin :left) (margin :right)))
+(def tree-height (- svg-height (margin :top) (margin :bottom)))
 
 
-;; var vis = d3.select("#body").append("svg:svg")
-;;    .attr("width", w + m[1] + m[3])
-;;    .attr("height", h + m[0] + m[2])
-;;  .append("svg:g")
-;;    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+(def vertical {:x-prop "x" :y-prop "y" :x0-prop "x0" :y0-prop "y0" :dx-prop "dx"})
+(def horizontal {:x-prop "y" :y-prop "x" :x0-prop "y0" :y0-prop "x0" :dx-prop "dy"})
+
+(def orientation vertical)
 
 
-(def vis (-> d3 (.select "#body") (.append "svg:svg")
-           (.attr {:width (+ w (m 1) (m 3))
-                   :height (+ h (m 0) (m 2))})
-           
+
+(def line (-> d3 (.-svg) (.line) (.x (fn [d] (aget d (orientation :x-prop)))) (.y (fn [d] (aget d (orientation :y-prop))))))
+(def line-segment (fn [spec]
+            (let [source (aget spec "source")
+                  target (aget spec "target")
+                  data [source target]]
+              (line data))))
+(def bezier (-> d3 (.-svg) (.diagonal)
+                (.projection (fn [d] [(aget d (orientation :x-prop)) (aget d (orientation :y-prop))]))))
+
+(def draw-link line-segment)
+
+
+(def svg (-> d3 (.select "#body") (.append "svg:svg")
+           (.attr "width" svg-width)
+           (.attr "height" svg-height)
            (.append "svg:g")
-           (.attr {:transform (str "translate(" (m 3) "," (m 0) ")")})))
-
-;; // Toggle children.
-;; function toggle(d) {
-;;  if (d.children) {
-;;    d._children = d.children;
-;;    d.children = null;
-;;  } else {
-;;    d.children = d._children;
-;;    d._children = null;
-;;  }
+           (.attr "transform" (str "translate(" (margin :left) "," (margin :top) ")"))))
 
 
 (defn toggle [d]
-  (if (.-children d)
+  (if (aget d "children")
     (do
-      (set! (.-_children d) (.-children d))
-      (set! (.-children d) nil))
+      (aset d "hidden-children" (aget d "children"))
+      (aset d "children" nil))
     (do
-      (set! (.-children d) (.-_children d))
-      (set! (.-_children d) nil))))
-
-
-;;  function toggleAll(d) {
-;;    if (d.children) {
-;;      d.children.forEach(toggleAll);
-;;      toggle(d);
-;;    }
-;;  }
+      (aset d "children" (aget d "hidden-children"))
+      (aset d "hidden-children" nil))))
 
 
 (defn toggle-all [d]
-  (when (.-children d)
-    (do
-      (doseq [child (.-children d)]
+  (when (aget d "children")
+      (doseq [child (aget d "children")]
         (toggle-all child))
-      (toggle d))))
+      (toggle d)))
 
 
-
-;; This piece replaces the global var i.
-;; See its use on line 131 below.
 (def next-node-id 0)
 (defn get-next-node-id []
   (set! next-node-id (inc next-node-id))
   next-node-id)
 
 
-
-
-;; function update(source) {
-
-(defn update [source]
+(defn update [source root tree]
   
-  
-;;  var duration = d3.event && d3.event.altKey ? 5000 : 500;
-  
-  (let [duration (if (and (.-event d3) (.-altKey (.-event d3)))
+  (let [duration (if (and (aget d3 "event") (aget (aget d3 "event") "altKey"))
                    5000
                    500)
         
+        click-handler (fn [d] (toggle d) (update d root tree))
+        
 ;;  // Compute the new tree layout.
-;;  var nodes = tree.nodes(root).reverse();
-
-        nodes (.reverse (.nodes tree root))
-
-;;  // Normalize for fixed-depth.
-;;  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-        _ (doseq [node nodes]
-            (fn [d] (set! (.-y d) (* (.-depth d) 180))))
+        nodes (.reverse ((aget tree "nodes") root))
 
 ;;  // Update the nodes…
-;;  var node = vis.selectAll("g.node")
-;;      .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-        node (-> vis (.selectAll "g.node")
+        node (-> svg (.selectAll "g.node")
                (.data nodes (fn [d]
-                              (when (not (.-id d))
-                                  (set! (.-id d) (get-next-node-id)))
-                              (.-id d))))
-
+                              (when (not (aget d "id"))
+                                  (aset d "id" (get-next-node-id)))
+                              (aget d "id"))))
 
 ;;  // Enter any new nodes at the parent's previous position.
-;;  var nodeEnter = node.enter().append("svg:g")
-;;      .attr("class", "node")
-;;      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-;;      .on("click", function(d) { toggle(d); update(d); });
-
         nodeEnter (-> node (.enter) (.append "svg:g")
-                    (.attr {:class "node"
-                            :transform (fn [d] (str "translate(" (.-y0 source) "," (.-x0 source) ")"))})
-                    (.on "click" (fn [d] (toggle d) (update d))))
-
-;;  nodeEnter.append("svg:circle")
-;;      .attr("r", 1e-6)
-;;      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-        _ (-> nodeEnter (.append "svg:circle")
-            (.attr {:r 1e-6})
-            (.style {:fill (fn [d] (if (.-_children d) "lightsteelblue" "#fff"))}))
-
-;;  nodeEnter.append("svg:text")
-;;      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-;;      .attr("dy", ".35em")
-;;      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-;;      .text(function(d) { return d.name; })
-;;      .style("fill-opacity", 1e-6);
-
-        _ (-> nodeEnter (.append "svg:text")
-            (.attr {:x (fn [d] (if (or (.-children d) (.-_children d)) -10 10))
-                    :dy ".35em"
-                    :text-anchor (fn [d] (if (or (.-children d) (.-_children d)) "end" "start"))})
-            (.text #(.-name %))
-            (.style {:fill-opacity 1e-6}))
+                    (.attr "class" "node")
+                    (.attr "transform" #(str "translate(" (aget source (orientation :x0-prop)) "," (aget source (orientation :y0-prop)) ")"))
+                    (.on "click" click-handler))
 
 ;;  // Transition nodes to their new position.
-;;  var nodeUpdate = node.transition()
-;;      .duration(duration)
-;;      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
         nodeUpdate (-> node (.transition)
                      (.duration duration)
-                     (.attr {:transform (fn [d] (str "translate(" (.-y d) "," (.-x d) ")"))}))
-
-;;  nodeUpdate.select("circle")
-;;      .attr("r", 4.5)
-;;      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-        _ (-> nodeUpdate (.select "circle")
-            (.attr {:r 4.5})
-            (.style {:fill (fn [d] (if (.-_children d) "lightsteelblue" "#fff"))}))
-
-;;  nodeUpdate.select("text")
-;;      .style("fill-opacity", 1);
-
-        _ (-> nodeUpdate (.select "text")
-            (.style {:fill-opacity 1}))
+                     (.attr "transform" #(str "translate(" (aget % (orientation :x-prop)) "," (aget % (orientation :y-prop)) ")")))
 
 ;;  // Transition exiting nodes to the parent's new position.
-;;  var nodeExit = node.exit().transition()
-;;      .duration(duration)
-;;      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-;;      .remove();
-
-        nodeExit (-> node (.exit) (.transition)
+        nodeExit (-> node (.exit) (.transition) ;; externed exit!
                    (.duration duration)
-                   (.attr {:transform (fn [d] (str "translate(" (.-y source) "," (.-x source) ")"))})
+                   (.attr "transform" #(str "translate(" (aget source (orientation :x-prop)) "," (aget source (orientation :y-prop)) ")"))
                    (.remove))
 
-;;  nodeExit.select("circle")
-;;      .attr("r", 1e-6);
-
-        _ (-> nodeExit (.select "circle")
-            (.attr {:r 1e-6}))
-
-;;  nodeExit.select("text")
-;;      .style("fill-opacity", 1e-6);
-
-        _ (-> nodeExit (.select "text")
-            (.style {:fill-opacity 1e-6}))
-
 ;;  // Update the links…
-;;  var link = vis.selectAll("path.link")
-;;      .data(tree.links(nodes), function(d) { return d.target.id; });
+        link (-> svg (.selectAll "path.link")
+               (.data (.links tree nodes) #(aget (aget % "target") "id")))]
 
-        link (-> vis (.selectAll "path.link")
-               (.data (.links tree nodes) (fn [d] (.-id (.-target d)))))
+;;  // Normalize for fixed-depth.
+        (doseq [node nodes]
+          #(aset % (orientation :y-prop) (* (aget % "depth") 10)))
+
+
+        (-> nodeEnter (.append "svg:circle")
+          (.attr "r" 1e-6)
+          (.style "fill" #(if (aget % "hidden-children") "lightsteelblue" "#fff")))
+
+        (-> nodeEnter (.append "svg:text")
+          (.attr (orientation :y-prop) #(if (or (aget % "children") (aget % "hidden-children")) -10 10))
+          (.attr (orientation :dx-prop) ".35em")
+          (.attr "text-anchor" #(if (or (aget % "children") (aget % "hidden-children")) "end" "start"))
+          (.text #(aget % "name"))
+          (.style "fill-opacity" 1e-6))
+
+        (-> nodeUpdate (.select "circle")
+          (.attr "r" 4.5)
+          (.style "fill" #(if (aget % "hidden-children") "lightsteelblue" "#fff")))
+
+        (-> nodeUpdate (.select "text")
+          (.style "fill-opacity" 1))
+
+        (-> nodeExit (.select "circle")
+          (.attr "r" 1e-6))
+
+        (-> nodeExit (.select "text")
+          (.style "fill-opacity 1e-6"))
 
 ;;  // Enter any new links at the parent's previous position.
-;;  link.enter().insert("svg:path", "g")
-;;      .attr("class", "link")
-;;      .attr("d", function(d) {
-;;        var o = {x: source.x0, y: source.y0};
-;;        return diagonal({source: o, target: o});
-;;      })
-;;    .transition()
-;;      .duration(duration)
-;;      .attr("d", diagonal);
-
-        _ (-> link (.enter) (.insert "svg:path" "g")
-            (.attr {:class "link"
-                    :d (fn [d]
-                         (let [o {:x (.-x0 source) :y (.-y0 source)}]
-                                 (diagonal {:source o :target o})))})
-            (.transition)
-            (.duration duration)
-            (.attr {:d diagonal}))
+        (-> link (.enter) (.insert "svg:path" "g")
+          (.attr "class" "link")
+          (.attr "d" #(let [o (js-obj "x" (aget source "x0") "y" (aget source "y0"))]
+                        (draw-link (js-obj "source" o "target" o))))
+          (.transition)
+          (.duration duration)
+          (.attr "d" draw-link))
 
 ;;  // Transition links to their new position.
-;;  link.transition()
-;;      .duration(duration)
-;;      .attr("d", diagonal);
-
-        _ (-> link (.transition)
-            (.duration duration)
-            (.attr {:d diagonal}))
+        (-> link (.transition)
+          (.duration duration)
+          (.attr "d" draw-link))
 
 ;;  // Transition exiting nodes to the parent's new position.
-;;  link.exit().transition()
-;;      .duration(duration)
-;;      .attr("d", function(d) {
-;;        var o = {x: source.x, y: source.y};
-;;        return diagonal({source: o, target: o});
-;;      })
-;;      .remove();
-
-        _ (-> link (.exit) (.transition)
-            (.duration duration)
-             (.attr {:d (fn [d]
-                          (let [o {:x (.-x source) :y (.-y source)}]
-                                  (diagonal {:source o :target o})))})
-             (.remove))
+        (-> link (.exit) (.transition)
+          (.duration duration)
+          (.attr "d" #(let [o (js-obj "x" (aget source "x") "y" (aget source "y"))]
+                        (draw-link (js-obj "source" o "target" o))))
+          (.remove))
 
 ;;  // Stash the old positions for transition.
-;;  nodes.forEach(function(d) {
-;;    d.x0 = d.x;
-;;    d.y0 = d.y;
-;;  });
-
-        _ (doseq [node nodes]
-            (set! (.-x0 node) (.-x node))
-            (set! (.-y0 node) (.-y node)))]
-;;}
-
-    ))
+        (doseq [node nodes]
+          (aset node "x0" (aget node "x"))
+          (aset node "y0" (aget node "y")))))
 
 
-;; d3.json("flare.json", function(json) {
-;;  root = json;
-;;  root.x0 = h / 2;
-;;  root.y0 = 0;
+(defn draw [json]
+  (let [root json
+        tree (-> d3 (.-layout) (.tree)
+               (.size (if (== orientation vertical) [tree-width tree-height] [tree-height tree-width])))]
+    (aset root "x0" (/ tree-width 2))
+    (aset root "y0" 0)
+                           
+;;  Initialize the display to show a few nodes.
+    (doseq [child (aget root "children")]
+      (toggle-all child))
+    (let [child (aget (aget root "children") 1)]
+      (toggle child)
+      (toggle (aget (aget child "children") 2)))
+    (let [child (aget (aget root "children") 9)]
+      (toggle child)
+      (toggle (aget (aget child "children") 0)))
+    
+    (update root root tree)))
 
-;;  // Initialize the display to show a few nodes.
-;;  root.children.forEach(toggleAll);
-;;  toggle(root.children[1]);
-;;  toggle(root.children[1].children[2]);
-;;  toggle(root.children[9]);
-;;  toggle(root.children[9].children[0]);
-
-;;  update(root);
-;; });
-
-
-(.json d3 "flare.json" (fn [json]
-                         (set! root json)
-                         (set! (.-x0 root) (/ h 2))
-                         (set! (.-y0 root) 0)
-                         
-                         (doseq [child (.-children root)]
-                           (toggle-all child))
-                         (let [child (aget (.-children root) 1)]
-                           (toggle child)
-                           (toggle (aget (.-children child) 2)))
-                         (let [child (aget (.-children root) 9)]
-                           (toggle child)
-                           (toggle (aget (.-children child) 0)))
-                         
-                         (update root)))
+(.json d3 "flare.json" draw)
