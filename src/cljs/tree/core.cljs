df39e4a4ed95fc55ef5f561cb8916bf8b2c902c2
Alan Shaw <alan@nodename.com>
2013-05-31 23:47:40 -0700

    The two layers (nodes and links) now separated each into its own redraw function

--- src/cljs/tree/core.cljs
+++ src/cljs/tree/core.cljs
@@ -88,30 +88,19 @@
 
 ;; redraw refactoring as in http://weblog.bocoup.com/reusability-with-d3
 
-(defn redraw [source-node zipper tree]
-  
-  (let [duration (if (and (aget d3 "event") (aget (aget d3 "event") "altKey"))
-                   5000
-                   500)
+(defn redraw-nodes [source-node tree nodes duration click-handler]
+  (let [fill-color #(if (aget % "hidden-children") "lightsteelblue" "#fff")
         
-        root (zip/root zipper)
-        
-        click-handler (fn [d] (toggle d) (redraw d zipper tree))
-        
-        ;; Compute the new tree layout.
-        nodes (.reverse ((aget tree "nodes") root))
-        
-        ;; Update the nodes…
         node (-> tree-canvas (.selectAll "g.node")
                (.data nodes (fn [d]
                               (when (not (aget d "id"))
                                   (aset d "id" (get-next-node-id)))
                               (aget d "id"))))
-
-        entering-nodes (-> node (.enter) (.append "svg:g"))
+        
+        entering (-> node (.enter) (.append "svg:g"))
         
         ;; Enter any new nodes at source-node's previous position.
-        node-enter (fn [node]
+        enter (fn [node]
                      (-> node
                        (.attr "class" "node")
                        (.attr "transform" #(str "translate(" (aget source-node (orientation :x0-prop)) "," (aget source-node (orientation :y0-prop)) ")"))
@@ -119,7 +108,7 @@
                      (let [circle (-> node (.append "svg:circle"))]
                        (-> circle
                          (.attr "r" 1e-6)
-                         (.style "fill" #(if (aget % "hidden-children") "lightsteelblue" "#fff"))))
+                         (.style "fill" fill-color)))
                      (let [text (-> node (.append "svg:text"))]
                        (-> text
                          (.attr (orientation :y-prop) #(if (or (aget % "children") (aget % "hidden-children")) -10 10))
@@ -128,26 +117,26 @@
                          (.text #(aget % "name"))
                          (.style "fill-opacity" 1e-6))))
         
-        updating-nodes-transition (-> node (.transition))
+        updating-transition (-> node (.transition))
         
         ;; Transition nodes to their new position.
-        node-transition-update (fn [transition]
+        transition-update (fn [transition]
                                  (-> transition
                                    (.duration duration)
                                    (.attr "transform" #(str "translate(" (aget % (orientation :x-prop)) "," (aget % (orientation :y-prop)) ")")))
                                  (let [circle (-> transition (.select "circle"))]
                                    (-> circle
                                      (.attr "r" 4.5)
-                                     (.style "fill" #(if (aget % "hidden-children") "lightsteelblue" "#fff"))))
+                                     (.style "fill" fill-color)))
                                  (let [text (-> transition (.select "text"))]
                                    (-> text
                                      (.style "fill-opacity" 1))))
                                  
         
-        exiting-nodes-transition (-> node (.exit) (.transition)) ;; externed exit!
+        exiting-transition (-> node (.exit) (.transition)) ;; externed exit!
         
         ;; Transition exiting nodes to source-node's new position.
-        node-transition-exit (fn [transition]
+        transition-exit (fn [transition]
                                (-> transition 
                                  (.duration duration)
                                  (.attr "transform" #(str "translate(" (aget source-node (orientation :x-prop)) "," (aget source-node (orientation :y-prop)) ")"))
@@ -157,70 +146,87 @@
                                    (.attr "r" 1e-6)))
                                (let [text (-> transition (.select "text"))]
                                  (-> text
-                                   (.style "fill-opacity 1e-6"))))
-                                 
-        
-        ;; Update the links…
-        link (-> tree-canvas (.selectAll "path.link")
+                                   (.style "fill-opacity 1e-6"))))]
+    
+    (-> entering enter)
+    
+    (-> updating-transition transition-update)
+    
+    (-> exiting-transition transition-exit)))
+
+
+(defn redraw-links [source-node tree nodes duration]
+  (let [link (-> tree-canvas (.selectAll "path.link")
                (.data (.links tree nodes) #(aget (aget % "target") "id")))
         
-        entering-links (-> link (.enter) (.insert "svg:path" "g"))
+        entering (-> link (.enter) (.insert "svg:path" "g"))
         
         ;; Enter any new links at source-node's previous position.
-        link-enter (fn [link]
+        enter (fn [link]
                      (-> link
                        (.attr "class" "link")
                        (.attr "d" #(let [o (js-obj "x" (aget source-node "x0") "y" (aget source-node "y0"))]
                                      (draw-link (js-obj "source" o "target" o))))))
         
-        entering-links-transition (-> entering-links (.transition))
+        entering-transition (-> entering (.transition))
         
-        link-transition-enter (fn [transition]
+        transition-enter (fn [transition]
                                 (-> transition
                                   (.duration duration)
                                   (.attr "d" draw-link)))
         
-        updating-links-transition (-> link (.transition))
+        updating-transition (-> link (.transition))
         
         ;; Transition links to their new position.
-        link-transition-update (fn [transition]
+        transition-update (fn [transition]
                                  (-> transition
                                    (.duration duration)
                                    (.attr "d" draw-link)))
         
-        exiting-links-transition (-> link (.exit) (.transition))
+        exiting-transition (-> link (.exit) (.transition))
         
         ;; Transition exiting links to source-node's new position.
-        link-transition-exit (fn [transition]
+        transition-exit (fn [transition]
                                (-> transition
                                  (.duration duration)
                                  (.attr "d" #(let [o (js-obj "x" (aget source-node "x") "y" (aget source-node "y"))]
                                                (draw-link (js-obj "source" o "target" o))))
                                  (.remove)))]
     
+    (-> entering enter)
     
+    (-> entering-transition transition-enter)
+    
+    (-> updating-transition transition-update)
+    
+    (-> exiting-transition transition-exit)))
+    
+
+        
+(defn redraw [source-node zipper tree]
+  
+  (let [duration (if (and (aget d3 "event") (aget (aget d3 "event") "altKey"))
+                   5000
+                   500)
+        
+        click-handler (fn [d] (toggle d) (redraw d zipper tree))
+        
+        root (zip/root zipper)
+        
+        ;; Compute the new tree layout.
+        nodes (.reverse ((aget tree "nodes") root))]
+        
     ;; Normalize for fixed-depth.
     (doseq [node nodes]
       #(aset % (orientation :y-prop) (* (aget % "depth") 10)))
     
-    (-> entering-nodes node-enter)
-    
-    (-> updating-nodes-transition node-transition-update)
-    
-    (-> exiting-nodes-transition node-transition-exit)
-    
-    (-> entering-links link-enter)
-    
-    (-> entering-links-transition link-transition-enter)
-    
-    (-> updating-links-transition link-transition-update)
-    
-    (-> exiting-links-transition link-transition-exit)
-    
     ;; Stash the old positions for transition.
     (doseq [node nodes]
       (aset node "x0" (aget node "x"))
-      (aset node "y0" (aget node "y")))))
+      (aset node "y0" (aget node "y")))
+    
+    (redraw-nodes source-node tree nodes duration click-handler)
+    (redraw-links source-node tree nodes duration)))
 
 
 
