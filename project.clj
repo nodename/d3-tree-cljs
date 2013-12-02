4f6ef387e0e52bcaa4c7edab602a756efe04b7e6
Alan Shaw <alan@nodename.com>
2013-12-01 22:09:22 -0800

    removed lein-simpleton

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
 (defproject tree "0.0.1-SNAPSHOT"
       :description "FIXME: write description"
       :dependencies [[org.clojure/clojure "1.5.1"]
-                     [net.drib/strokes "0.4.1"]]
+                     [net.drib/strokes "0.4.1"]
+                     ]
       :min-lein-version "2.0.0"
       :source-paths ["src/clj" "src/cljs"]
 
-      :plugins [[lein-cljsbuild "0.3.0"]
-                [lein-simpleton "1.1.0"]]
+      :plugins [[lein-cljsbuild "0.3.0"]]
 
       :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                            :compiler { :output-to "public/out/tree.js"
-                                        :pretty-print true 
-                                        :optimizations :simple}}]})
+                            :compiler {:output-to "public/out/tree.js"
+                                       :externs ["../strokes/public/d3/d3-externs.js"]
+                                       :pretty-print true
+                                    ;   :optimizations :advanced}}]})
+                                       :optimizations :simple}}]})
